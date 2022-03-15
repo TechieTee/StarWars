273a8ee5647e0017ce20a674f42f3875daec66c9
FatimaModupeBello <fatimab@hardcorebiometric.com>
2022-03-15 20:18:45 +0100

    modifies css on login form

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -8,8 +8,6 @@ import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
 import { useForm, Controller } from 'react-hook-form'
 
-
-
 const LoginPage = () => {
 	const navigate = useNavigate()
 	const { login, handleSetUser } = useLogin()
@@ -21,85 +19,74 @@ const LoginPage = () => {
 		handleSetUser(data)
 		navigate('/dashboard')
 	}
-
+	const style = { width: '100%', margin: '20px 0', padding: '4px 0' }
 	return (
 		<AppLayout login={true}>
-		<FormWrapper index>
-					<h3>Login</h3>
-					<h4>Kindly enter your details to log in</h4>
-					<form onSubmit={handleSubmit(onSubmit)}>
-						<Controller
-							name='email'
-							control={control}
-							render={({
-								field: { onChange, value },
-								fieldState: { error },
-							}) => (
-								<TextField
-									label='Email Address'
-									variant='outlined'
-									value={value}
-									defaultValue=''
-									onChange={onChange}
-									error={!!error}
-									helperText={error ? error.message : null}
-									type='email'
-								/>
-							)}
-							rules={{
-								required: 'Email is required',
-								pattern: {
-									value:
-										/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-									message: 'Please enter a valid email',
-								},
-							}}
-						/>
+			<FormWrapper index>
+				<h3>Login</h3>
+				<h4>Kindly enter your details to log in</h4>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<Controller
+						name='email'
+						control={control}
+						render={({ field: { onChange, value }, fieldState: { error } }) => (
+							<TextField
+								style={style}
+								label='Email Address'
+								variant='outlined'
+								value={value}
+								defaultValue=''
+								onChange={onChange}
+								error={!!error}
+								helperText={error ? error.message : null}
+								type='email'
+							/>
+						)}
+						rules={{
+							required: 'Email is required',
+							pattern: {
+								value:
+									/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+								message: 'Please enter a valid email',
+							},
+						}}
+					/>
 
-						<Controller
-							name='password'
-							control={control}
-							render={({
-								field: { onChange, value },
-								fieldState: { error },
-							}) => (
-								<TextField
-									label='Password'
-									variant='outlined'
-									value={value}
-									onChange={onChange}
-									defaultValue=''
-									error={!!error}
-									helperText={error ? error.message : null}
-									type='password'
-								/>
-							)}
-							rules={{ required: 'Password required' }}
-						/>
+					<Controller
+						name='password'
+						control={control}
+						render={({ field: { onChange, value }, fieldState: { error } }) => (
+							<TextField
+								style={style}
+								label='Password'
+								variant='outlined'
+								value={value}
+								onChange={onChange}
+								defaultValue=''
+								error={!!error}
+								helperText={error ? error.message : null}
+								type='password'
+							/>
+						)}
+						rules={{ required: 'Password required' }}
+					/>
 
-						<Button
-							type='submit'
-							variant='contained'
-							color={'primary'}
-							style={{ width: '110%' }}
-						>
-							Log In
-						</Button>
-						<h5>forgot your password?</h5>
-					</form>
-					<h6>
-						Privacy Policy <span>and</span> Terms of services
-					</h6>
-				</FormWrapper>
+					<Button
+						type='submit'
+						variant='contained'
+						color={'primary'}
+						style={style}
+					>
+						Log In
+					</Button>
+					<h5>forgot your password?</h5>
+				</form>
+				<h6>
+					Privacy Policy <span>and</span> Terms of services
+				</h6>
+			</FormWrapper>
 		</AppLayout>
-	
 	)
 }
 
 export default LoginPage
-
-
-
-			
-
-			
